5fc0ae49bea394803f259b1da520487e40ac1467
Emilis <bblom9178@gmail.com>
2025-03-25 20:20:54 +0200

    Added keybinding and categories with a english lang support

--- src/client/java/net/daycounter/DayCounterClient.java
+++ src/client/java/net/daycounter/DayCounterClient.java
@@ -1,18 +1,43 @@
 package net.daycounter;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import net.minecraft.util.Util;
 import net.minecraft.util.math.ColorHelper;
 import net.minecraft.util.math.MathHelper;
+import org.lwjgl.glfw.GLFW;
 
 public class DayCounterClient implements ClientModInitializer {
+	private static boolean showDayHUD = true;
+	private static boolean showFPSHUD = true;
+	private static KeyBinding toggleDayKey;
+	private static KeyBinding toggleFPSKey;
+	private static String category = "category.daycounter";
+
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 		HudRenderCallback.EVENT.register(((drawContext, renderTickCounter) -> onHudRender(drawContext)));
+
+		toggleDayKey = new KeyBinding("key.daycounter.toggleDay", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_Z, category);
+		toggleFPSKey = new KeyBinding("key.daycounter.toggleFPS", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_V, category);
+
+		KeyBindingHelper.registerKeyBinding(toggleDayKey);
+		KeyBindingHelper.registerKeyBinding(toggleFPSKey);
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (toggleDayKey.wasPressed()) {
+				showDayHUD = !showDayHUD;
+			}
+			if (toggleFPSKey.wasPressed()) {
+				showFPSHUD = !showFPSHUD;
+			}
+		});
 	}
 
 	private void onHudRender(DrawContext drawContext) {
@@ -37,8 +62,17 @@ public class DayCounterClient implements ClientModInitializer {
 			int lerpedRedToGreen = ColorHelper.lerp(lerpedAmount, red, green);
 			int lerpedGreenToBlue = ColorHelper.lerp(lerpedAmount, green, blue);
 
-			drawContext.drawTextWithShadow(client.textRenderer, "Day: " + day, DayX, DayY, lerpedGreenToBlue);
-			drawContext.drawTextWithShadow(client.textRenderer, fps + " FPS", FpsX, FpsY, lerpedRedToGreen);
+			if (showDayHUD) {
+				drawContext.drawTextWithShadow(client.textRenderer, "Day: " + day, DayX, DayY, lerpedGreenToBlue);
+			}
+
+			if (showFPSHUD) {
+				if (!showDayHUD) {
+					drawContext.drawTextWithShadow(client.textRenderer, fps + " FPS", DayX, DayY, lerpedRedToGreen);
+				} else {
+					drawContext.drawTextWithShadow(client.textRenderer, fps + " FPS", FpsX, FpsY, lerpedRedToGreen);
+				}
+			}
 		}
 	}
 }
